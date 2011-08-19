449b8d2b8b97534e45982000146ccce1f675efcd
Xinhao.Yuan <xinhaoyuan@gmail.com>
2011-08-20 01:48:29 +0800

    ...

--- mk/comphost.mk
+++
@@ -1,6 +0,0 @@
-CC := gcc
-LD := ld
-
-T_CC_BASE_FLAGS  := 
-T_CC_OPT_FLAGS   := -O0
-T_CC_DEBUG_FLAGS := -g
--- mk/compk32.mk
+++
@@ -1,7 +0,0 @@
-CC      := gcc -fno-builtin -fno-builtin-function -nostdinc
-LD		:= ld
-OBJDUMP	:= objdump
-OBJCOPY := objcopy
-STRIP   := strip
-
-include ${T_BASE}/mk/compopt.mk
