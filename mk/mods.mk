59a3ea75b15876988da331db46d4cec40957d7ef
Xinhao.Yuan <xinhaoyuan@gmail.com>
2011-08-28 17:00:59 +0800

    code cleaning

--- mk/mods.mk
+++ mk/mods.mk
@@ -4,7 +4,7 @@ E_ENCODE ?= $(shell echo $(1) | sed -e 's!_!_1!g' -e 's!/!_2!g')
 E_DECODE ?= $(shell echo $(1) | sed -e 's!_1!_!g' -e 's!_2!/!g')
 
 # The order makes sense
-MODS := libs-sv supervisor libs-kern kern ht-sign ht-mksfs libs-user user bootloader
+MODS := libs-sv supervisor kern ht-sign ht-mksfs libs-user user bootloader
 
 MODDIRS := $(addprefix mod-,${MODS})
 
